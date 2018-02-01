53aea4106ebf3ca9672fe460a0742bd118410aa2
Pascal Laporte <plaporte@nuglif.com>
2018-01-31 22:11:17 -0500

    JENKINS-49295 Proper template pattern for readTrusted

--- src/main/java/com/cloudbees/jenkins/plugins/bitbucket/server/client/BitbucketServerAPIClient.java
+++ src/main/java/com/cloudbees/jenkins/plugins/bitbucket/server/client/BitbucketServerAPIClient.java
@@ -117,7 +117,7 @@ public class BitbucketServerAPIClient implements BitbucketApi {
     private static final String API_BRANCHES_PATH = API_REPOSITORY_PATH + "/branches{?start,limit}";
     private static final String API_PULL_REQUESTS_PATH = API_REPOSITORY_PATH + "/pull-requests{?start,limit}";
     private static final String API_PULL_REQUEST_PATH = API_REPOSITORY_PATH + "/pull-requests/{id}";
-    private static final String API_BROWSE_PATH = API_REPOSITORY_PATH + "/browse{/path}{?at}";
+    private static final String API_BROWSE_PATH = API_REPOSITORY_PATH + "/browse/{+path}{?at}";
     private static final String API_COMMITS_PATH = API_REPOSITORY_PATH + "/commits{/hash}";
     private static final String API_PROJECT_PATH = API_BASE_PATH + "/projects/{owner}";
     private static final String API_COMMIT_COMMENT_PATH = API_REPOSITORY_PATH + "/commits{/hash}/comments";
