9fa1b9eaa7c9b0ccec34b4859c485ccb46b09dce
Joseph Petersen <josephp90@gmail.com>
2020-01-28 21:40:06 +0100

    imitate github branch source plugin

--- src/main/java/com/cloudbees/jenkins/plugins/bitbucket/BitbucketSCMNavigator.java
+++ src/main/java/com/cloudbees/jenkins/plugins/bitbucket/BitbucketSCMNavigator.java
@@ -222,12 +222,36 @@ public class BitbucketSCMNavigator extends SCMNavigator {
         this.credentialsId = Util.fixEmpty(credentialsId);
     }
 
+    /**
+     * Sets the behavioural traits that are applied to this navigator and any {@link BitbucketSCMSource} instances it
+     * discovers. The new traits will take affect on the next navigation through any of the
+     * {@link #visitSources(SCMSourceObserver)} overloads or {@link #visitSource(String, SCMSourceObserver)}.
+     *
+     * @param traits the new behavioural traits.
+     */
+    @SuppressWarnings({"unchecked", "rawtypes"})
     @DataBoundSetter
-    @Override
-    public void setTraits(@CheckForNull List<SCMTrait<?>> traits) {
+    public void setTraits(@CheckForNull SCMTrait[] traits) {
         // the reduced generics in the method signature are a workaround for JENKINS-26535
-        //noinspection unchecked,rawtypes
-        this.traits = new ArrayList<>((List)/*defensive*/Util.fixNull(traits));
+        this.traits = new ArrayList<>();
+        if (traits != null) {
+            for (SCMTrait trait : traits) {
+                this.traits.add(trait);
+            }
+        }
+    }
+
+    /**
+     * Sets the behavioural traits that are applied to this navigator and any {@link BitbucketSCMSource} instances it
+     * discovers. The new traits will take affect on the next navigation through any of the
+     * {@link #visitSources(SCMSourceObserver)} overloads or {@link #visitSource(String, SCMSourceObserver)}.
+     *
+     * @param traits the new behavioural traits.
+     */
+    @Override
+    public void setTraits(@CheckForNull List<SCMTrait<? extends SCMTrait<?>>> traits) {
+        this.traits = traits != null ? new ArrayList<>(traits) : new ArrayList<SCMTrait<? extends SCMTrait<?>>>();
+
     }
 
     public String getServerUrl() {
