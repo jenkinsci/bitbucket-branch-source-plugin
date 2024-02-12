jenkinsci/bitbucket-branch-source-plugin

commit 28d74e8b4226bfc7524b412e34f7090784cc1a08

Support cloning from Bitbucket mirror (#796)
A mirrored Git repository can be configured for fetching references.

The mirror is not used in the following cases:
- If the source branch in a pull request resides in a different
  repository, the source branch is fetched from the primary
  repository while the target branch is fetched from the mirror.
- During initial pull request scanning, the mirror isn't used because
  PullRequestSCMHead doesn't contain the hash needed for the build and
  SCMRevision is null. This is the current design limitation and
  can be refactored later.

Cloning from the mirror can only be used with native web-hooks since
plugin web-hooks don't provide a mirror identifier.

For branches and tags, the mirror sync event is used. Thus, at cloning
time, the mirror is already synchronized. However, in the case of a
pull request event, there is no such guarantee. The plugin
optimistically assumes that the mirror is synced and the required
commit hashes exist in the mirrored repository at cloning time. If the
plugin can't find the required hashes, it falls back to the primary repository.

Fixes #592

Co-authors:
- Andrey Fomin https://github.com/andrey-fomin
- Andrei Kouznetchik https://github.com/akouznetchik
- Eugene Mercuriev https://github.com/zamonier

Co-authored-by: Günter Grodotzki <gunter@grodotzki.com>

src/main/java/com/cloudbees/jenkins/plugins/bitbucket/BitbucketGitSCMBuilder.java
@@ -23,11 +23,9 @@
  */
 package com.cloudbees.jenkins.plugins.bitbucket;
 
-import com.cloudbees.jenkins.plugins.bitbucket.api.BitbucketApi;
 import com.cloudbees.jenkins.plugins.bitbucket.api.BitbucketHref;
 import com.cloudbees.jenkins.plugins.bitbucket.api.BitbucketRepository;
 import com.cloudbees.jenkins.plugins.bitbucket.api.BitbucketRepositoryProtocol;
-import com.cloudbees.jenkins.plugins.bitbucket.client.BitbucketCloudApiClient;
 import com.cloudbees.jenkins.plugins.bitbucket.endpoints.AbstractBitbucketEndpoint;
 import com.cloudbees.jenkins.plugins.bitbucket.endpoints.BitbucketEndpointConfiguration;
 import com.cloudbees.jenkins.plugins.bitbucket.endpoints.BitbucketServerEndpoint;
@@ -40,12 +38,8 @@ import edu.umd.cs.findbugs.annotations.NonNull;
 import hudson.Util;
 import hudson.plugins.git.GitSCM;
 import hudson.plugins.git.browser.BitbucketWeb;
-import java.util.ArrayList;
-import java.util.Collections;
 import java.util.List;
-import jenkins.plugins.git.AbstractGitSCMSource;
 import jenkins.plugins.git.GitSCMBuilder;
-import jenkins.plugins.git.MergeWithGitSCMExtension;
 import jenkins.scm.api.SCMHead;
 import jenkins.scm.api.SCMRevision;
 import jenkins.scm.api.SCMSource;
@@ -68,11 +62,16 @@ public class BitbucketGitSCMBuilder extends GitSCMBuilder<BitbucketGitSCMBuilder
     private final BitbucketSCMSource scmSource;
 
     /**
-     * The clone links for cloning the source repository and origin pull requests (but links will need tweaks for
-     * fork pull requests)
+     * The clone links for primary repository
      */
     @NonNull
-    private List<BitbucketHref> cloneLinks = Collections.emptyList();
+    private List<BitbucketHref> primaryCloneLinks = List.of();
+
+    /**
+     * The clone links for mirror repository if it's configured
+     */
+    @NonNull
+    private List<BitbucketHref> mirrorCloneLinks = List.of();
 
     /**
      * The {@link BitbucketRepositoryProtocol} that should be used.
@@ -96,21 +95,6 @@ public class BitbucketGitSCMBuilder extends GitSCMBuilder<BitbucketGitSCMBuilder
         // we provide a dummy repository URL to the super constructor and then fix is afterwards once we have
         // the clone links
         super(head, revision, /*dummy value*/scmSource.getServerUrl(), credentialsId);
-        withoutRefSpecs();
-        if (head instanceof PullRequestSCMHead) {
-            if (scmSource.buildBitbucketClient() instanceof BitbucketCloudApiClient) {
-                // TODO fix once Bitbucket Cloud has a fix for https://bitbucket.org/site/master/issues/5814
-                String branchName = ((PullRequestSCMHead) head).getBranchName();
-                withRefSpec("+refs/heads/" + branchName + ":refs/remotes/@{remote}/" + head.getName());
-            } else {
-                String pullId = ((PullRequestSCMHead) head).getId();
-                withRefSpec("+refs/pull-requests/" + pullId + "/from:refs/remotes/@{remote}/" + head.getName());
-            }
-        } else if (head instanceof TagSCMHead ){
-            withRefSpec("+refs/tags/" + head.getName() + ":refs/tags/" + head.getName());
-        } else {
-            withRefSpec("+refs/heads/" + head.getName() + ":refs/remotes/@{remote}/" + head.getName());
-        }
         this.scmSource = scmSource;
         AbstractBitbucketEndpoint endpoint =
                 BitbucketEndpointConfiguration.get().findEndpoint(scmSource.getServerUrl());
@@ -130,11 +114,19 @@ public class BitbucketGitSCMBuilder extends GitSCMBuilder<BitbucketGitSCMBuilder
     /**
      * Provides the clone links from the {@link BitbucketRepository} to allow inference of ports for different protocols.
      *
-     * @param cloneLinks the clone links.
+     * @param primaryCloneLinks the clone links for primary repository.
+     * @param mirrorCloneLinks the clone links for mirror repository if it's configured.
      * @return {@code this} for method chaining.
      */
-    public BitbucketGitSCMBuilder withCloneLinks(List<BitbucketHref> cloneLinks) {
-        this.cloneLinks = new ArrayList<>(Util.fixNull(cloneLinks));
+    public BitbucketGitSCMBuilder withCloneLinks(
+        @CheckForNull List<BitbucketHref> primaryCloneLinks,
+        @CheckForNull List<BitbucketHref> mirrorCloneLinks
+    ) {
+        if (primaryCloneLinks == null) {
+            throw new IllegalArgumentException("Primary clone links shouldn't be null");
+        }
+        this.primaryCloneLinks = primaryCloneLinks;
+        this.mirrorCloneLinks = Util.fixNull(mirrorCloneLinks);
         return withBitbucketRemote();
     }
 
@@ -147,16 +139,6 @@ public class BitbucketGitSCMBuilder extends GitSCMBuilder<BitbucketGitSCMBuilder
     @NonNull
     public BitbucketSCMSource scmSource() {
         return scmSource;
-    }
-
-    /**
-     * Returns the clone links (possibly empty).
-     *
-     * @return the clone links (possibly empty).
-     */
-    @NonNull
-    public List<BitbucketHref> cloneLinks() {
-        return Collections.unmodifiableList(cloneLinks);
     }
 
     /**
@@ -206,70 +188,130 @@ public class BitbucketGitSCMBuilder extends GitSCMBuilder<BitbucketGitSCMBuilder
      */
     @NonNull
     public BitbucketGitSCMBuilder withBitbucketRemote() {
-        SCMHead h = head();
-        String repoOwner;
-        String repository;
-        BitbucketApi bitbucket = scmSource().buildBitbucketClient();
-        if (h instanceof PullRequestSCMHead && bitbucket instanceof BitbucketCloudApiClient) {
-            // TODO fix once Bitbucket Cloud has a fix for https://bitbucket.org/site/master/issues/5814
-            repoOwner = ((PullRequestSCMHead) h).getRepoOwner();
-            repository = ((PullRequestSCMHead) h).getRepository();
+        SCMHead head = head();
+        withoutRefSpecs();
+        String headName = head.getName();
+        if (head instanceof PullRequestSCMHead) {
+            withPullRequestRemote((PullRequestSCMHead) head, headName);
+        } else if (head instanceof TagSCMHead) {
+            withTagRemote(headName);
         } else {
-            // head instanceof BranchSCMHead
-            repoOwner = scmSource.getRepoOwner();
-            repository = scmSource.getRepository();
-        }
-
-        String cloneLink = null;
-        for (BitbucketHref link : cloneLinks()) {
-            if (protocol.getType().equals(link.getName())) {
-                cloneLink = link.getHref();
-                break;
-            }
-        }
-        withRemote(bitbucket.getRepositoryUri(
-                protocol,
-                cloneLink,
-                repoOwner,
-                repository));
-        AbstractBitbucketEndpoint endpoint =
-                BitbucketEndpointConfiguration.get().findEndpoint(scmSource.getServerUrl());
-        if (endpoint == null) {
-            endpoint = new BitbucketServerEndpoint(null, scmSource.getServerUrl(), false, null);
-        }
-        withBrowser(new BitbucketWeb(
-                endpoint.getRepositoryUrl(
-                        repoOwner,
-                        repository
-                )));
-
-        // now, if we have to build a merge commit, let's ensure we build the merge commit!
-        SCMRevision r = revision();
-        if (h instanceof PullRequestSCMHead) {
-            PullRequestSCMHead head = (PullRequestSCMHead) h;
-            if (head.getCheckoutStrategy() == ChangeRequestCheckoutStrategy.MERGE) {
-                String name = head.getTarget().getName();
-                String localName = head.getBranchName().equals(name) ? "upstream-" + name : name;
-
-                String remoteName = remoteName().equals("upstream") ? "upstream-upstream" : "upstream";
-                withAdditionalRemote(remoteName,
-                        bitbucket.getRepositoryUri(
-                                protocol,
-                                cloneLink,
-                                scmSource().getRepoOwner(),
-                                scmSource().getRepository()),
-                        "+refs/heads/" + name + ":refs/remotes/@{remote}/" + localName);
-                if ((r instanceof PullRequestSCMRevision)
-                        && ((PullRequestSCMRevision) r).getTarget() instanceof AbstractGitSCMSource.SCMRevisionImpl) {
-                    withExtension(new MergeWithGitSCMExtension("remotes/" + remoteName + "/" + localName,
-                            ((AbstractGitSCMSource.SCMRevisionImpl) ((PullRequestSCMRevision) r).getTarget())
-                                    .getHash()));
-                } else {
-                    withExtension(new MergeWithGitSCMExtension("remotes/" + remoteName + "/" + localName, null));
-                }
-            }
+            withBranchRemote(headName);
         }
         return this;
+    }
+
+    private void withPullRequestRemote(PullRequestSCMHead head, String headName) {
+        String scmSourceRepoOwner = scmSource.getRepoOwner();
+        String scmSourceRepository = scmSource.getRepository();
+        String pullRequestRepoOwner = head.getRepoOwner();
+        String pullRequestRepository = head.getRepository();
+        boolean prFromTargetRepository = pullRequestRepoOwner.equals(scmSourceRepoOwner)
+            && pullRequestRepository.equals(scmSourceRepository);
+        SCMRevision revision = revision();
+        ChangeRequestCheckoutStrategy checkoutStrategy = head.getCheckoutStrategy();
+        // PullRequestSCMHead should be refactored to add references to target and source commit hashes.
+        // So revision should not be used here. There is a hack to use revision to get hashes.
+        boolean cloneFromMirror = prFromTargetRepository
+            && !mirrorCloneLinks.isEmpty()
+            && revision instanceof PullRequestSCMRevision;
+        String targetBranch = head.getTarget().getName();
+        String branchName = head.getBranchName();
+        if (prFromTargetRepository) {
+            withRefSpec("+refs/heads/" + branchName + ":refs/remotes/@{remote}/" + branchName);
+            if (cloneFromMirror) {
+                PullRequestSCMRevision pullRequestSCMRevision = (PullRequestSCMRevision) revision;
+                String primaryRemoteName = remoteName().equals("primary") ? "primary-primary" : "primary";
+                String cloneLink = getCloneLink(primaryCloneLinks);
+                List<BranchWithHash> branchWithHashes;
+                if (checkoutStrategy == ChangeRequestCheckoutStrategy.MERGE) {
+                    branchWithHashes = List.of(
+                        new BranchWithHash(branchName, pullRequestSCMRevision.getPull().getHash()),
+                        new BranchWithHash(targetBranch, pullRequestSCMRevision.getTargetImpl().getHash())
+                    );
+                } else {
+                    branchWithHashes = List.of(
+                        new BranchWithHash(branchName, pullRequestSCMRevision.getPull().getHash())
+                    );
+                }
+                withExtension(new FallbackToOtherRepositoryGitSCMExtension(cloneLink, primaryRemoteName, branchWithHashes));
+                withMirrorRemote();
+            } else {
+                withPrimaryRemote();
+            }
+        } else {
+            if (scmSource.isCloud()) {
+                withRefSpec("+refs/heads/" + branchName + ":refs/remotes/@{remote}/" + headName);
+                String cloneLink = getCloudRepositoryUri(pullRequestRepoOwner, pullRequestRepository);
+                withRemote(cloneLink);
+            } else {
+                String pullId = head.getId();
+                withRefSpec("+refs/pull-requests/" + pullId + "/from:refs/remotes/@{remote}/" + headName);
+                withPrimaryRemote();
+            }
+        }
+        if (head.getCheckoutStrategy() == ChangeRequestCheckoutStrategy.MERGE) {
+            String hash = revision instanceof PullRequestSCMRevision
+                ? ((PullRequestSCMRevision) revision).getTargetImpl().getHash()
+                : null;
+            String refSpec = "+refs/heads/" + targetBranch + ":refs/remotes/@{remote}/" + targetBranch;
+            if (!prFromTargetRepository && scmSource.isCloud()) {
+                String upstreamRemoteName = remoteName().equals("upstream") ? "upstream-upstream" : "upstream";
+                withAdditionalRemote(upstreamRemoteName, getCloneLink(primaryCloneLinks), refSpec);
+                withExtension(new MergeWithGitSCMExtension("remotes/" + upstreamRemoteName + "/" + targetBranch, hash));
+            } else {
+                withRefSpec(refSpec);
+                withExtension(new MergeWithGitSCMExtension("remotes/" + remoteName() + "/" + targetBranch, hash));
+            }
+        }
+    }
+
+    @NonNull
+    public String getCloudRepositoryUri(@NonNull String owner, @NonNull String repository) {
+        switch (protocol) {
+            case HTTP:
+                return "https://bitbucket.org/" + owner + "/" + repository + ".git";
+            case SSH:
+                return "ssh://git@bitbucket.org/" + owner + "/" + repository + ".git";
+            default:
+                throw new IllegalArgumentException("Unsupported repository protocol: " + protocol);
+        }
+    }
+
+    private void withTagRemote(String headName) {
+        withRefSpec("+refs/tags/" + headName + ":refs/tags/" + headName);
+        if (mirrorCloneLinks.isEmpty()) {
+            withPrimaryRemote();
+        } else {
+            withMirrorRemote();
+        }
+    }
+
+    private void withBranchRemote(String headName) {
+        withRefSpec("+refs/heads/" + headName + ":refs/remotes/@{remote}/" + headName);
+        if (mirrorCloneLinks.isEmpty()) {
+            withPrimaryRemote();
+        } else {
+            withMirrorRemote();
+        }
+    }
+
+    private void withPrimaryRemote() {
+        String cloneLink = getCloneLink(primaryCloneLinks);
+        withRemote(cloneLink);
+    }
+
+    private void withMirrorRemote() {
+        String cloneLink = getCloneLink(mirrorCloneLinks);
+        withRemote(cloneLink);
+    }
+
+    private String getCloneLink(List<BitbucketHref> cloneLinks) {
+        return cloneLinks.stream()
+            .filter(link -> protocol.getType().equals(link.getName()))
+            .findAny()
+            .orElseThrow(() -> new IllegalStateException("Can't find clone link for protocol " + protocol))
+            .getHref();
     }
 
     /**

commit 2fff462b623b013cfb7678e2124092d9dcf8da56

Properly handle value "https" in clone link name
In Bitbucket Server "http" is used as http link name,
but in Bitbucket Cloud "https" is used.

Bug was introduced in #796. Fixes #804.

src/main/java/com/cloudbees/jenkins/plugins/bitbucket/BitbucketGitSCMBuilder.java
@@ -308,7 +308,7 @@ public class BitbucketGitSCMBuilder extends GitSCMBuilder<BitbucketGitSCMBuilder
 
     private String getCloneLink(List<BitbucketHref> cloneLinks) {
         return cloneLinks.stream()
-            .filter(link -> protocol.getType().equals(link.getName()))
+            .filter(link -> protocol.matches(link.getName()))
             .findAny()
             .orElseThrow(() -> new IllegalStateException("Can't find clone link for protocol " + protocol))
             .getHref();

src/main/java/com/cloudbees/jenkins/plugins/bitbucket/api/BitbucketRepositoryProtocol.java
@@ -23,7 +23,6 @@
  */
 package com.cloudbees.jenkins.plugins.bitbucket.api;
 
-import edu.umd.cs.findbugs.annotations.CheckForNull;
 import edu.umd.cs.findbugs.annotations.NonNull;
 
 public enum BitbucketRepositoryProtocol {
@@ -44,18 +43,16 @@ public enum BitbucketRepositoryProtocol {
         this.type = type;
     }
 
-    public String getType() {
-        return type;
+    /**
+     * Check if link name matches protocol.
+     * In Bitbucket Server "http" and "ssh" are used as link names.
+     * In Bitbucket Cloud "https" and "ssh" are used.
+     *
+     * @param linkName link name to check
+     * @return if link name matches
+     */
+    public boolean matches(@NonNull String linkName) {
+        return linkName.startsWith(type);
     }
 
-    @CheckForNull
-    public static BitbucketRepositoryProtocol fromString(String type) {
-        if (SSH.type.equals(type)) {
-            return SSH;
-        } else if (HTTP.type.equals(type)) {
-            return HTTP;
-        } else {
-            return null;
-        }
-    }
 }

src/test/java/com/cloudbees/jenkins/plugins/bitbucket/BitbucketGitSCMBuilderTest.java
@@ -93,7 +93,7 @@ public class BitbucketGitSCMBuilderTest {
 
         instance.withCloneLinks(
             List.of(
-                new BitbucketHref("http", "https://bitbucket.org/tester/test-repo.git"),
+                new BitbucketHref("https", "https://bitbucket.org/tester/test-repo.git"),
                 new BitbucketHref("ssh", "ssh://git@bitbucket.org/tester/test-repo.git")
             ),
             List.of()
@@ -153,7 +153,7 @@ public class BitbucketGitSCMBuilderTest {
 
         instance.withCloneLinks(
             List.of(
-                new BitbucketHref("http", "https://bitbucket.org/tester/test-repo.git"),
+                new BitbucketHref("https", "https://bitbucket.org/tester/test-repo.git"),
                 new BitbucketHref("ssh", "ssh://git@bitbucket.org/tester/test-repo.git")
             ),
             List.of()
@@ -213,7 +213,7 @@ public class BitbucketGitSCMBuilderTest {
 
         instance.withCloneLinks(
             List.of(
-                new BitbucketHref("http", "https://bitbucket.org/tester/test-repo.git"),
+                new BitbucketHref("https", "https://bitbucket.org/tester/test-repo.git"),
                 new BitbucketHref("ssh", "ssh://git@bitbucket.org/tester/test-repo.git")
             ),
             List.of()
@@ -271,7 +271,7 @@ public class BitbucketGitSCMBuilderTest {
 
         instance.withCloneLinks(
             List.of(
-                new BitbucketHref("http", "https://bitbucket.org/tester/test-repo.git"),
+                new BitbucketHref("https", "https://bitbucket.org/tester/test-repo.git"),
                 new BitbucketHref("ssh", "ssh://git@bitbucket.org/tester/test-repo.git")
             ),
             List.of()
@@ -317,7 +317,7 @@ public class BitbucketGitSCMBuilderTest {
 
         instance.withCloneLinks(
             List.of(
-                new BitbucketHref("http", "https://bitbucket.org/tester/test-repo.git"),
+                new BitbucketHref("https", "https://bitbucket.org/tester/test-repo.git"),
                 new BitbucketHref("ssh", "ssh://git@bitbucket.org/tester/test-repo.git")
             ),
             List.of()
@@ -363,7 +363,7 @@ public class BitbucketGitSCMBuilderTest {
 
         instance.withCloneLinks(
             List.of(
-                new BitbucketHref("http", "https://bitbucket.org/tester/test-repo.git"),
+                new BitbucketHref("https", "https://bitbucket.org/tester/test-repo.git"),
                 new BitbucketHref("ssh", "ssh://git@bitbucket.org/tester/test-repo.git")
             ),
             List.of()
@@ -915,7 +915,7 @@ public class BitbucketGitSCMBuilderTest {
 
         instance.withCloneLinks(
             List.of(
-                new BitbucketHref("http", "https://bitbucket.org/tester/test-repo.git"),
+                new BitbucketHref("https", "https://bitbucket.org/tester/test-repo.git"),
                 new BitbucketHref("ssh", "ssh://git@bitbucket.org/tester/test-repo.git")
             ),
             List.of()
@@ -979,7 +979,7 @@ public class BitbucketGitSCMBuilderTest {
 
         instance.withCloneLinks(
             List.of(
-                new BitbucketHref("http", "https://bitbucket.org/tester/test-repo.git"),
+                new BitbucketHref("https", "https://bitbucket.org/tester/test-repo.git"),
                 new BitbucketHref("ssh", "ssh://git@bitbucket.org/tester/test-repo.git")
             ),
             List.of()
@@ -1042,7 +1042,7 @@ public class BitbucketGitSCMBuilderTest {
 
         instance.withCloneLinks(
             List.of(
-                new BitbucketHref("http", "https://bitbucket.org/tester/test-repo.git"),
+                new BitbucketHref("https", "https://bitbucket.org/tester/test-repo.git"),
                 new BitbucketHref("ssh", "ssh://git@bitbucket.org/tester/test-repo.git")
             ),
             List.of()
@@ -1106,7 +1106,7 @@ public class BitbucketGitSCMBuilderTest {
 
         instance.withCloneLinks(
             List.of(
-                new BitbucketHref("http", "https://bitbucket.org/tester/test-repo.git"),
+                new BitbucketHref("https", "https://bitbucket.org/tester/test-repo.git"),
                 new BitbucketHref("ssh", "ssh://git@bitbucket.org/tester/test-repo.git")
             ),
             List.of()
@@ -1176,7 +1176,7 @@ public class BitbucketGitSCMBuilderTest {
 
         instance.withCloneLinks(
             List.of(
-                new BitbucketHref("http", "https://bitbucket.org/tester/test-repo.git"),
+                new BitbucketHref("https", "https://bitbucket.org/tester/test-repo.git"),
                 new BitbucketHref("ssh", "ssh://git@bitbucket.org/tester/test-repo.git")
             ),
             List.of()
@@ -1246,7 +1246,7 @@ public class BitbucketGitSCMBuilderTest {
 
         instance.withCloneLinks(
             List.of(
-                new BitbucketHref("http", "https://bitbucket.org/tester/test-repo.git"),
+                new BitbucketHref("https", "https://bitbucket.org/tester/test-repo.git"),
                 new BitbucketHref("ssh", "ssh://git@bitbucket.org/tester/test-repo.git")
             ),
             List.of()
@@ -1316,7 +1316,7 @@ public class BitbucketGitSCMBuilderTest {
 
         instance.withCloneLinks(
             List.of(
-                new BitbucketHref("http", "https://bitbucket.org/tester/test-repo.git"),
+                new BitbucketHref("https", "https://bitbucket.org/tester/test-repo.git"),
                 new BitbucketHref("ssh", "ssh://git@bitbucket.org/tester/test-repo.git")
             ),
             List.of()
@@ -1386,7 +1386,7 @@ public class BitbucketGitSCMBuilderTest {
 
         instance.withCloneLinks(
             List.of(
-                new BitbucketHref("http", "https://bitbucket.org/tester/test-repo.git"),
+                new BitbucketHref("https", "https://bitbucket.org/tester/test-repo.git"),
                 new BitbucketHref("ssh", "ssh://git@bitbucket.org/tester/test-repo.git")
             ),
             List.of()
@@ -1456,7 +1456,7 @@ public class BitbucketGitSCMBuilderTest {
 
         instance.withCloneLinks(
             List.of(
-                new BitbucketHref("http", "https://bitbucket.org/tester/test-repo.git"),
+                new BitbucketHref("https", "https://bitbucket.org/tester/test-repo.git"),
                 new BitbucketHref("ssh", "ssh://git@bitbucket.org/tester/test-repo.git")
             ),
             List.of()
@@ -1522,7 +1522,7 @@ public class BitbucketGitSCMBuilderTest {
 
         instance.withCloneLinks(
             List.of(
-                new BitbucketHref("http", "https://bitbucket.org/tester/test-repo.git"),
+                new BitbucketHref("https", "https://bitbucket.org/tester/test-repo.git"),
                 new BitbucketHref("ssh", "ssh://git@bitbucket.org/tester/test-repo.git")
             ),
             List.of()
@@ -1570,7 +1570,7 @@ public class BitbucketGitSCMBuilderTest {
 
         instance.withCloneLinks(
             List.of(
-                new BitbucketHref("http", "https://bitbucket.org/tester/test-repo.git"),
+                new BitbucketHref("https", "https://bitbucket.org/tester/test-repo.git"),
                 new BitbucketHref("ssh", "ssh://git@bitbucket.org/tester/test-repo.git")
             ),
             List.of()
@@ -1618,7 +1618,7 @@ public class BitbucketGitSCMBuilderTest {
 
         instance.withCloneLinks(
             List.of(
-                new BitbucketHref("http", "https://bitbucket.org/tester/test-repo.git"),
+                new BitbucketHref("https", "https://bitbucket.org/tester/test-repo.git"),
                 new BitbucketHref("ssh", "ssh://git@bitbucket.org/tester/test-repo.git")
             ),
             List.of()
@@ -2484,7 +2484,7 @@ public class BitbucketGitSCMBuilderTest {
 
         instance.withCloneLinks(
             List.of(
-                new BitbucketHref("http", "https://bitbucket.org/tester/test-repo.git"),
+                new BitbucketHref("https", "https://bitbucket.org/tester/test-repo.git"),
                 new BitbucketHref("ssh", "ssh://git@bitbucket.org/tester/test-repo.git")
             ),
             List.of()
@@ -2568,7 +2568,7 @@ public class BitbucketGitSCMBuilderTest {
 
         instance.withCloneLinks(
             List.of(
-                new BitbucketHref("http", "https://bitbucket.org/tester/test-repo.git"),
+                new BitbucketHref("https", "https://bitbucket.org/tester/test-repo.git"),
                 new BitbucketHref("ssh", "ssh://git@bitbucket.org/tester/test-repo.git")
             ),
             List.of()
@@ -2652,7 +2652,7 @@ public class BitbucketGitSCMBuilderTest {
 
         instance.withCloneLinks(
             List.of(
-                new BitbucketHref("http", "https://bitbucket.org/tester/test-repo.git"),
+                new BitbucketHref("https", "https://bitbucket.org/tester/test-repo.git"),
                 new BitbucketHref("ssh", "ssh://git@bitbucket.org/tester/test-repo.git")
             ),
             List.of()
@@ -2732,7 +2732,7 @@ public class BitbucketGitSCMBuilderTest {
 
         instance.withCloneLinks(
             List.of(
-                new BitbucketHref("http", "https://bitbucket.org/tester/test-repo.git"),
+                new BitbucketHref("https", "https://bitbucket.org/tester/test-repo.git"),
                 new BitbucketHref("ssh", "ssh://git@bitbucket.org/tester/test-repo.git")
             ),
             List.of()
@@ -2800,7 +2800,7 @@ public class BitbucketGitSCMBuilderTest {
 
         instance.withCloneLinks(
             List.of(
-                new BitbucketHref("http", "https://bitbucket.org/tester/test-repo.git"),
+                new BitbucketHref("https", "https://bitbucket.org/tester/test-repo.git"),
                 new BitbucketHref("ssh", "ssh://git@bitbucket.org/tester/test-repo.git")
             ),
             List.of()