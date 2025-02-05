a12d7eef9f19b52939f26b36ec3ae573f9c427a0
Allan Burdajewicz <aburdajewicz@cloudbees.com>
2025-02-06 09:40:57 +1000

    [JENKINS-55927] Scan if Bitbucket Cloud otherwise check property

--- src/main/java/com/cloudbees/jenkins/plugins/bitbucket/hooks/PushHookProcessor.java
+++ src/main/java/com/cloudbees/jenkins/plugins/bitbucket/hooks/PushHookProcessor.java
@@ -59,7 +59,7 @@ public class PushHookProcessor extends HookProcessor {
                 if (push.getChanges().isEmpty()) {
                     final String owner = push.getRepository().getOwnerName();
                     final String repository = push.getRepository().getRepositoryName();
-                    if (instanceType == BitbucketType.SERVER && SCAN_ON_PUSH_WITH_EMPTY_CHANGES) {
+                    if (instanceType == BitbucketType.CLOUD || SCAN_ON_PUSH_WITH_EMPTY_CHANGES) {
                         LOGGER.log(Level.INFO, "Received push hook with empty changes from Bitbucket. Processing indexing on {0}/{1}",
                             new Object[]{owner, repository});
                         scmSourceReIndex(owner, repository);
