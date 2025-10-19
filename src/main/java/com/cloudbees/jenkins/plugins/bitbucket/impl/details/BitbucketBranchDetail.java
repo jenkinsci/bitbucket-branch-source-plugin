7ba55502f9adc0446b554244aa2b135450bdf5ab
Nikolas Falco <nfalco79@hotmail.com>
2025-10-19 16:30:47 +0200

    [JENKINS-76215] Support for Details Widget
    WIP, not clear where these details should be shown, refactory repository details using a GitBrowser instance from GitSCM

---
+++ src/main/java/com/cloudbees/jenkins/plugins/bitbucket/impl/details/BitbucketBranchDetail.java
@@ -0,0 +1,66 @@
+/*
+ * The MIT License
+ *
+ * Copyright (c) 2025, Nikolas Falco
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+ */
+package com.cloudbees.jenkins.plugins.bitbucket.impl.details;
+
+import edu.umd.cs.findbugs.annotations.Nullable;
+import hudson.model.Actionable;
+import hudson.model.Run;
+import jenkins.model.details.Detail;
+import jenkins.model.details.DetailGroup;
+import jenkins.scm.api.SCMDetailGroup;
+import jenkins.scm.api.metadata.ObjectMetadataAction;
+
+public class BitbucketBranchDetail extends Detail {
+
+    public BitbucketBranchDetail(Actionable object) {
+        super(object);
+    }
+
+    @Nullable
+    @Override
+    public String getIconClassName() {
+        return "symbol-git-branch-outline plugin-ionicons-api";
+    }
+
+    @Nullable
+    @Override
+    public String getDisplayName() {
+        return getObjectMetadataAction().getObjectDisplayName();
+    }
+
+    @Override
+    public String getLink() {
+        return getObjectMetadataAction().getObjectUrl();
+    }
+
+    @Override
+    public DetailGroup getGroup() {
+        return SCMDetailGroup.get();
+    }
+
+    private ObjectMetadataAction getObjectMetadataAction() {
+        Run<?, ?> run = (Run<?, ?>) getObject();
+        return run.getParent().getAction(ObjectMetadataAction.class);
+    }
+}
